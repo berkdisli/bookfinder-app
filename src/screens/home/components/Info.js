862733457ea075714b88402d238c7ba018efacc9
Berk Disli <berkdisli@yahoo.com>
2022-12-01 10:23:18 +0100

    react redux, app.js update, Info.js

---
+++ src/screens/home/components/Info.js
@@ -0,0 +1,8 @@
+
+const Info = ({ query, totalItems }) = {
+    return(
+        <h3> Search results for: { query }</h3 >
+    <p>Total results: {totalItems}</p>
+    );
+    }
+export default Info;
